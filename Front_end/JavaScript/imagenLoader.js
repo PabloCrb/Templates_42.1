f69cccd9e6b24fc98d23cc0e7cfd238de515cd9c
PabloCrb <zerytv75@gmail.com>
2024-03-25 17:27:46 +0000

    Account carga de JSON

--- Front_end/JavaScript/imagenLoader.js
+++ Front_end/JavaScript/imagenLoader.js
@@ -1,4 +1,4 @@
-function addImage(contenedor, imagenPath, texto) {
+function addImage(imagenPath, texto) {
     let nuevoElemento = document.createElement('div');
     nuevoElemento.className = 'imagewd';
     nuevoElemento.style.width = '350px';
@@ -29,21 +29,104 @@ function addImage(contenedor, imagenPath, texto) {
     });
 
     nuevoElemento.appendChild(textoElemento);
+    let contenedor = document.getElementById("imagenContainer");
     contenedor.appendChild(nuevoElemento);
 }
 
-function cargarImagenes() {
-    const contenedorImagenes = document.getElementById('imagenContainer');
-    const imagenesYTextos = [
-        { imagenPath: '../Images/prueba1.jpg', texto: 'Estadio de Las Palmas de Gran Canaria'},
-        { imagenPath: '../Images/prueba2.jpg', texto: 'Casa de Colón'},
-        { imagenPath: '../Images/prueba3.jpg', texto: 'Roque Bentaiga'},
-        { imagenPath: '../Images/prueba4.jpg', texto: 'Presa de las niñas'},
-        { imagenPath: '../Images/prueba5.jpg', texto: 'Club náutico de Las Palmas de G.C'},
-        { imagenPath: '../Images/prueba6.jpg', texto: 'Barranco de Azuaje'}
-    ];
+ async function loadUserDirectory(url) {
+    const userID = localStorage.getItem("userName");
 
-    imagenesYTextos.forEach(item => {
-        addImage(contenedorImagenes, item.imagenPath, item.texto);
-    });
+    if (!userID) {
+        console.error('User name not found in localStorage.');
+        return Promise.reject('User name not found in localStorage.');
+    }
+
+    return fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const filteredUser = data["Users"].filter(user => user.userName === userID);
+            if (filteredUser.length === 1) {
+                return filteredUser[0];
+            } else {
+                throw new Error('User not found or multiple users found with the same ID.');
+            }
+        })
+        .then(data => {
+            return data["directory"];
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+            throw error;
+        });
 }
+
+async function getUserPublicationsIDs(url, fieldName) {
+    return fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const publicationList = data[fieldName];
+            return publicationList.map(publication => publication.publication_id);
+        });
+}
+
+async function getPublicationURL(id) {
+    return fetch("/Front_end/Prueba_carga_dinamica/publication.JSON")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const publications = data["Publication"];
+            if (id >= 1 && id <= publications.length) {
+                const index = id - 1;
+                return publications[index].url;
+            } else {
+                throw new Error('Publication ID out of range');
+            }
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+            throw error;
+        });
+}
+
+async function loadPublicationField(url, fieldName) {
+    return fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            return data[fieldName];
+        })
+        .then(data => {
+            return (fieldName === "name") ? data : data[0];
+        });
+}
+
+(async () => {
+    localStorage.setItem("userName", "maria@maria.es");
+    const directory = await loadUserDirectory("prueba_user.JSON");
+    const publication_id = await getUserPublicationsIDs(directory, "my_publication_list");
+
+    for (const id of publication_id) {
+        const publication_url = await getPublicationURL(id);
+        const imagen = await loadPublicationField(publication_url, "image_list");
+        const name = await loadPublicationField(publication_url, "name");
+        addImage(imagen, name);
+        }
+})();
