d71687ba289abdcc73ea571f5842d656939db019
Pedro Manuel <pedrom.rodriguezhernandez@gmail.com>
2024-03-28 18:12:30 +0000

    Publication Done

--- Front_end/JavaScript/loadPublication.js
+++ Front_end/JavaScript/loadPublication.js
@@ -21,6 +21,7 @@ function initializeMap(latitude, longitude) {
     return map;
 }
 
+
 async function takeUrl(publicationID) {
     const response = await fetch("/Front_end/Prueba_carga_dinamica/publication.json")
         .then(async response => {
@@ -32,20 +33,31 @@ async function takeUrl(publicationID) {
     const publication = response.Publication.find(pub => pub.id === publicationID);
     return publication ? publication.url : null;
 }
-
-
+function createScore(valoration) {
+    const valorationElement = document.getElementById("score");
+    valorationElement.innerHTML = valoration;
+    const valorationValue = parseFloat(valoration);
+    const roundedValue = Math.floor(valorationValue);
+    for (let i = 0; i < roundedValue; i++) {
+        const star = document.createElement('span');
+        star.className = 'star';
+        star.innerHTML = '&#9733;'; // Unicode de estrella
+        valorationElement.appendChild(star);
+    }
+}
 async function loadElements(data) {
     document.getElementById("name").textContent = data.name;
+    createScore(data.score);
     document.getElementById("description").textContent = data.description;
-    const userName = await fetch("/Front_end/Prueba_carga_dinamica/Json/Users/" + data.user_id + ".json")
+    const user = await fetch("/Front_end/Prueba_carga_dinamica/Json/Users/" + data.user_id + ".json")
         .then(async response => {
             if (!response.ok) {
                 throw new Error("Fail to fetch document");
             }
             return await response.json()
         });
-    console.log(userName);
-    document.getElementById("username").textContent = userName.user;
+    document.getElementById("username").textContent = user.user;
+    document.getElementById("photoUser").src = user.user_image;
 
 }
 
@@ -55,6 +67,45 @@ async function loadImages(image_list) {
         const nuevaImagen = document.createElement('img');
         nuevaImagen.src = imageUrl;
         listaImagenes.appendChild(nuevaImagen);
+    }
+}
+
+function createComment(userName, photoUser, text) {
+    var comentariosDiv = document.getElementById('coments');
+    var comentarioHTML = `
+                <div class="comentario">
+                    <div class="header-comment">
+                        <img src="${photoUser}" class="imagen-comentario"> 
+                        <h3 class="nombre">${userName}</h3>
+                    </div>
+                    <div class="mensaje">${text}</div>
+                </div>
+            `;
+    comentariosDiv.innerHTML += comentarioHTML;
+}
+
+async function loadComment(comment_list) {
+    console.log(comment_list);
+    for ( comment of comment_list){
+        const response = await fetch(comment)
+            .then(response =>{
+                if (!response.ok){
+                    throw new Error("Can not load comment");
+                }
+                return response.json();
+            });
+        const user = await fetch("/Front_end/Prueba_carga_dinamica/Json/Users/" + response.user_id + ".json")
+            .then(async response => {
+                if (!response.ok) {
+                    throw new Error("Fail to fetch document");
+                }
+                return await response.json()
+            });
+        var userName = user.user;
+        var photoUser = user.user_image;
+        var text = response.text;
+        createComment(userName,photoUser,text);
+
     }
 }
 
@@ -70,5 +121,6 @@ async function initializePublication() {
     loadElements(response);
     initializeMap(response.location[0], response.location[1]);
     await loadImages(response.image_list);
+    await loadComment(response.comment_list);
 
 }
